922d111dcd30c372f784f4e045fa3bc5bda9bc7b
Cyril <pichard.cyril@laposte.net>
2013-10-19 17:21:52 +0100

    first implemention of fileSequenceCopy and fileSequenceMove

--- src/System/FileSequence/Manip.hs
+++ src/System/FileSequence/Manip.hs
@@ -5,6 +5,9 @@ module System.FileSequence.Manip where
 
 import System.FileSequence
 import System.Directory
+import System.Posix.Files
+import System.Posix.IO
+import Control.Monad 
 
 -- |Remove all frames of the sequence
 fileSequenceRemove :: FileSequence -> IO ()
@@ -14,23 +17,51 @@ fileSequenceRemove fs_ =
 -- | Copy a all files of a file sequence to a new directory
 -- Returns the newly generated filesequence (TODO maybe ? or error)
 fileSequenceCopy :: FileSequence -> FilePath -> IO FileSequence
-fileSequenceCopy fs_ path_ = return fs_
-    --if path fs_ == path_ 
-    --    then return fs_ 
-    --    else 
-    --        -- build the dest FileSequence
-    --        --let destFs = fs_ {path=path_}
-    --        --map frameName (frameRange fs) fs
-    --        return fs_
+fileSequenceCopy fs_ path_ = do
+    dirExists <- doesDirectoryExist path_
+    when (not dirExists) $ error ("Directory " ++ show path_ ++ " does not exist")
+    pathSrc <- canonicalizePath $ path fs_
+    pathDst <- canonicalizePath path_
+    if pathSrc == pathDst
+        then return fs_
+        else let fsr_ = fs_ {path=pathDst} in
+             do mapM_ (uncurry copyIfExist) $ zip (frameList fs_) (frameList fsr_) 
+                return fsr_
+    where copyIfExist sf_ df_ = do 
+             exist <- doesFileExist sf_
+             when exist (copyFile sf_ df_)
+          
 -- |
 fileSequenceMove :: FileSequence -> FilePath -> IO FileSequence
-fileSequenceMove fs_ path_ = return fs_
+fileSequenceMove fs_ path_ = do
+    dirExists <- doesDirectoryExist path_
+    when (not dirExists) $ error ("Directory " ++ show fs_ ++ " does not exist")
+    pathSrc <- canonicalizePath $ path fs_
+    pathDst <- canonicalizePath path_
+    if pathSrc == pathDst
+        then return fs_
+        else let fsr_ = fs_ {path=pathDst} in
+             do mapM_ (uncurry renameIfExist) $ zip (frameList fs_) (frameList fsr_) 
+                return fsr_
+    where renameIfExist sf_ df_ = do 
+             exist <- doesFileExist sf_
+             when exist (renameFile sf_ df_)
 
 -- |
-fileSequenceRename :: FileSequence -> String -> IO FileSequence
-fileSequenceRename fs_ name_ = return fs_
+--fileSequenceRename :: FileSequence -> String -> IO FileSequence
+--fileSequenceRename fs_ name_ = return fs_
 
--- |
-fileSequenceTranslate :: FileSequence -> FileSequence -> IO FileSequence
-fileSequenceTranslate fsSrc_ = return 
+
+-- |Same functionnality as the touch unix command.
+fileSequenceTouch :: FileSequence -> IO FileSequence
+fileSequenceTouch fs_ = do 
+    mapM_ touch $ frameList fs_ 
+    -- TODO : recompute filesequence attributes like padding ?
+    return fs_
+    where touch f = do exists <- fileExist f
+                       if exists
+                        then touchFile f
+                        --  TODO : get the default permissions of the user
+                        else do fd <- createFile f ownerReadMode
+                                closeFd fd
 
